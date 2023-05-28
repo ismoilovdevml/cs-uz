fd139d3c2ce64a3b402efecc3e53e1eb56d4c953
ismilovdevml <ismoilovdevarchlinux@gmail.com>
2023-05-28 19:05:00 +0500

    [FIX] bugs fixed

--- components/react/docs/index.ts
+++ components/react/docs/index.ts
@@ -1,3 +1,3 @@
 export * from './Header';
-export * from './HeaderVideo';
+// export * from './HeaderVideo';
 export * from './player';
--- components/react/examples/index.ts
+++ components/react/examples/index.ts
@@ -2,7 +2,7 @@ export * from './AccessControl';
 export * from './AptosNft';
 export * from './Asset';
 export * from './DecentralizedStoragePlayback';
-export * from './DemoPlayer';
+// export * from './DemoPlayer';
 export * from './IframePlayer';
 export * from './Stream';
 export * from './VideoJSPlayer';
